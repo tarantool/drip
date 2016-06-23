392697505e59b661192cc568783f70219dc26597
Konstantin Nazarov <mail@racktear.com>
2016-06-23 12:49:30 +0300

    Simplify code

--- test.py
+++ test.py
@@ -3,42 +3,14 @@
 import unittest
 import ripnotify
 
-RIP_COMMAND_RESPONSE  = 2
-RIP_VERSION_2         = 2
-RIP_DEFAULT_PORT      = 520
-RIP_AUTH_MD5          = 3
-RIP_MD5_AUTH_LEN      = 16
-RIP_AUTH_TYPE_ID_MD5  = 0x0100
-RIP_ENTRY_MAX_RECORDS = 25
-RIP_METRIC_POISON     = 15
-
-
 def byte2hex(byteStr):
     return ''.join( [ "%02x" % ord( x ) for x in byteStr ] ).strip()
 
 class RIPTest(unittest.TestCase):
-    def test_header(self):
-
-        cmd = 1
-        ver = 1
-
-        h = ripnotify.RIPHeader(cmd=cmd, ver=ver)
-        p = h.serialize()
-        self.assertEquals(byte2hex(p), "01010000")
-
-        cmd = 1
-        ver = 2
-
-        h = ripnotify.RIPHeader(cmd=cmd, ver=ver)
-        p = h.serialize()
-        self.assertEquals(byte2hex(p), "01020000")
-
     def test_without_auth(self):
-        cmd = RIP_COMMAND_RESPONSE
-        version = RIP_VERSION_2
-
         rtes = [{'route_tag': 1337,
-                 'cidr': "222.173.190.239/32", # 0xdeadbeef
+                 'ip': "222.173.190.239", # 0xdeadbeef
+                 'mask': "255.255.255.255", #0xffffffff
                  'next_hop': "27.173.202.254", # 0x1badcafe
                  'metric': 123456}]
 
@@ -46,8 +18,7 @@ class RIPTest(unittest.TestCase):
         auth_type = None
         seqno = 56789
 
-        pkt = ripnotify.rip_packet(cmd, version, rtes,
-                                   seqno, password, auth_type)
+        pkt = ripnotify.rip_packet(rtes, seqno, password, auth_type)
 
         b = []
         for i in range(len(pkt)/4):
@@ -64,11 +35,9 @@ class RIPTest(unittest.TestCase):
 
 
     def test_plain_auth(self):
-        cmd = RIP_COMMAND_RESPONSE
-        version = RIP_VERSION_2
-
         rtes = [{'route_tag': 1337,
-                 'cidr': "222.173.190.239/32", # 0xdeadbeef
+                 'ip': "222.173.190.239", # 0xdeadbeef
+                 'mask': "255.255.255.255", #0xffffffff
                  'next_hop': "27.173.202.254", # 0x1badcafe
                  'metric': 123456}]
 
@@ -76,8 +45,7 @@ class RIPTest(unittest.TestCase):
         auth_type = "plain"
         seqno = 56789
 
-        pkt = ripnotify.rip_packet(cmd, version, rtes,
-                                   seqno, password, auth_type)
+        pkt = ripnotify.rip_packet(rtes, seqno, password, auth_type)
 
         b = []
         for i in range(len(pkt)/4):
@@ -100,12 +68,9 @@ class RIPTest(unittest.TestCase):
 
 
     def test_md5_auth(self):
-
-        cmd = RIP_COMMAND_RESPONSE
-        version = RIP_VERSION_2
-
         rtes = [{'route_tag': 1337,
-                 'cidr': "222.173.190.239/32", # 0xdeadbeef
+                 'ip': "222.173.190.239", # 0xdeadbeef
+                 'mask': "255.255.255.255", #0xffffffff
                  'next_hop': "27.173.202.254", # 0x1badcafe
                  'metric': 123456}]
 
@@ -113,8 +78,7 @@ class RIPTest(unittest.TestCase):
         auth_type = "md5"
         seqno = 56789
 
-        pkt = ripnotify.rip_packet(cmd, version, rtes,
-                                   seqno, password, auth_type)
+        pkt = ripnotify.rip_packet(rtes, seqno, password, auth_type)
 
         b = []
         for i in range(len(pkt)/4):
@@ -140,6 +104,32 @@ class RIPTest(unittest.TestCase):
         self.assertEquals(b[13], "4e55d34a")
         self.assertEquals(b[14], "2b58ab85")
         self.assertEquals(b[15], "8ae6580e")
+
+    def test_poison(self):
+        rtes = [{'route_tag': 1337,
+                 'ip': "222.173.190.239", # 0xdeadbeef
+                 'mask': "255.255.255.255", #0xffffffff
+                 'next_hop': "27.173.202.254", # 0x1badcafe
+                 'metric': ripnotify.RIP_METRIC_POISON}]
+
+        password = None
+        auth_type = None
+        seqno = 56789
+
+        pkt = ripnotify.rip_packet(rtes, seqno, password, auth_type)
+
+        b = []
+        for i in range(len(pkt)/4):
+            b.append(byte2hex(pkt[i*4:i*4+4]))
+
+        # header
+        self.assertEquals(b[0], "02020000")
+        # RTE entry
+        self.assertEquals(b[1], "00020539")
+        self.assertEquals(b[2], "deadbeef")
+        self.assertEquals(b[3], "ffffffff")
+        self.assertEquals(b[4], "1badcafe")
+        self.assertEquals(b[5], "0f000000")
 
 
 if __name__ == '__main__':
